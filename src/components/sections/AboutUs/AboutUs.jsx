5033aaf29ab6fe83be4843fc8417ad47b02f3f84
SheihMaks <92758256+SheihMaks@users.noreply.github.com>
2023-07-19 20:45:40 +0300

    +

--- src/components/sections/AboutUs/AboutUs.jsx
+++ src/components/sections/AboutUs/AboutUs.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import styles from "./AboutUs.module.scss"
 
 const AboutUs = () => {
-    return(<div></div>)
+    return (<div className={styles.aboutUsContainer}>
+        <h2>Cristofer Robbin</h2>
+        <p>The most popular developer under the age of 30, he has revolutionized the understanding of urban planning and has become the most desirable candidate for receiving orders</p>
+    </div>)
 }
 
 export default AboutUs;
