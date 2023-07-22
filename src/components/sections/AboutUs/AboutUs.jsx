c23d5649d4b43966bc5fdd2e977282259278eef9
SheihMaks <92758256+SheihMaks@users.noreply.github.com>
2023-07-22 18:37:11 +0300

    +

--- src/components/sections/AboutUs/AboutUs.jsx
+++ src/components/sections/AboutUs/AboutUs.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import styles from "./AboutUs.module.scss"
+import icons from "images/icons.svg"
 
 const AboutUs = () => {
-    return (<div className={styles.aboutUsContainer}>
+    return (<div className={styles.aboutUsSectionContainer}>
         <h2 id="aboutUs">Cristofer Robbin</h2>
-        <p>The most popular developer under the age of 30, he has revolutionized the understanding of urban planning and has become the most desirable candidate for receiving orders</p>
+            <p>The most popular developer under the age of 30, he has revolutionized the understanding of urban planning and has become the most desirable candidate for receiving orders</p>
+            <svg>
+                <use href={icons + "#icon-aboutUs"}/>
+            </svg>
     </div>)
 }
 
