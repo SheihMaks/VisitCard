6aecb409120b50a797a5ff22163c7fe6d7c67d5a
SheihMaks <92758256+SheihMaks@users.noreply.github.com>
2023-07-23 18:25:51 +0300

    +

--- src/components/sections/AboutUs/AboutUs.jsx
+++ src/components/sections/AboutUs/AboutUs.jsx
@@ -1,14 +1,12 @@
 import React from "react";
-import styles from "./AboutUs.module.scss"
-import icons from "images/icons.svg"
+import styles from "./AboutUs.module.scss";
+import aboutUs from "images/AboutUs.svg";
 
 const AboutUs = () => {
     return (<div className={styles.aboutUsSectionContainer}>
         <h2 id="aboutUs">Cristofer Robbin</h2>
             <p>The most popular developer under the age of 30, he has revolutionized the understanding of urban planning and has become the most desirable candidate for receiving orders</p>
-            <svg>
-                <use href={icons + "#icon-aboutUs"}/>
-            </svg>
+        <img src={aboutUs } alt="img"/>
     </div>)
 }
 
