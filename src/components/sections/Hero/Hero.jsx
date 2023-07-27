5943b6029532aac59a87816a6cf617da1dff03e9
SheihMaks <92758256+SheihMaks@users.noreply.github.com>
2023-07-27 14:22:15 +0300

    +

--- src/components/sections/Hero/Hero.jsx
+++ src/components/sections/Hero/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
         <div className={styles.navContainer}>
             <h1><a href="#contacts">Cristofer Robbin</a></h1>
             <ul className={styles.navLinks}>
-                <li><a href="ourWorks">works</a></li>
+                <li><a href="#ourWorks">works</a></li>
                 <li><a href="#aboutUs">about</a></li>
             </ul>
         </div>
